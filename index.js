e70f6405bdad31ce7fc073d46ddbe3a785bae1e6
MshSiam <siyamkenway@gmail.com>
2021-10-30 18:14:35 +0600

    post,get added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const { MongoClient } = require("mongodb")
+const cors = require("cors")
 require("dotenv").config()
+const ObjectId = require("mongodb").ObjectId
 
 const app = express()
 const port = process.env.PORT || 5000
+
+// set middlewere
+app.use(cors())
+app.use(express.json())
 
 // connecting database //
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.woosd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
@@ -12,6 +18,44 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
+//  creating Async function //
+
+async function run() {
+  try {
+    await client.connect()
+    const database = client.db("worldTour")
+    const tourSpot = database.collection("spots")
+
+    //--------GET api------- //
+    app.get("/spots", async (req, res) => {
+      const cursor = tourSpot.find({})
+      const spots = await cursor.toArray()
+      res.send(spots)
+    })
+
+    // ------Get a service according to _id------ //
+    app.get("/spots/:id", async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const spot = await tourSpot.findOne(query)
+      res.json(spot)
+    })
+
+    // --------POST api------ //
+    app.post("/spots", async (req, res) => {
+      const spot = req.body
+      //   console.log("api hitted", spot)
+      const result = await tourSpot.insertOne(spot)
+      console.log(result)
+      res.json(result)
+    })
+  } finally {
+    //   await client.close()
+  }
+}
+
+run().catch(console.dir)
 
 app.get("/", (req, res) => {
   res.send("Running Tour Server")
